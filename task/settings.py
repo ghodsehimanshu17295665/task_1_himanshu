4dd686cebd4290c86cad34b1a0ecfa9e4d0f7aa4
Himanshu <himanshughodse@gmail.com>
2024-12-11 17:50:53 +0530

    Modified settings file

--- task/settings.py
+++ task/settings.py
@@ -148,3 +148,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "app.User"
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "himanshughodse@gmail.com"
+EMAIL_HOST_PASSWORD = "yxkx rwjr jqlq tkmw"
